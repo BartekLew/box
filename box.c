2de698cdb3c37efd6ab350d7a3b19137f8c4cafd
Lev <barpas@zoho.com>
2018-11-11 13:45:33 +0000

    rewrite to use poll instead of parallel i/o for each stream

--- box.c
+++ box.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #include <fcntl.h>
+#include <poll.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -22,52 +23,6 @@ static void cleanup(int signal) {
 	unlink(output_fifo);
 	unlink(error_fifo);
 	exit(0);
-}
-
-/* Dear reader, this solution COULD cause lead to some nasty
-   situations if there are more than one breakable pipe per
-   process. Now I use fork() so I'm rather safe, because every
-   named pipe is handled in separate file. If I'd switch to
-   threads it could lead to some nasty conditions. :-D */
-bool pipbrk = false;
-static void brk_pipe_handler(int signal) {
-	pipbrk = true;
-}
-
-typedef struct {
-	bool read_break, write_break;
-} ForwardResult;
- 
-static ForwardResult forward_stream(int in, int out) {
-	static char buff[0x100];
-	static size_t buff_fill = 0;
-	size_t in_size;
-
-	pipbrk = false;
-
-	if(buff_fill > 0)
-		write(out, buff, buff_fill);
-
-	if (pipbrk) {
-		pipbrk = false;
-		return (ForwardResult){.write_break = true};
-	}
-
-	while((in_size = read(in, buff, 0x100))>0) {
-		if (pipbrk) {
-			pipbrk = false;
-			return (ForwardResult){.read_break = true};
-		}
-
-		write(out, buff, in_size);
-		if(pipbrk) {
-			buff_fill = in_size;
-			pipbrk = false;
-			return (ForwardResult){.write_break = true};
-		}
-	}
-
-	return (ForwardResult){.read_break = false, .write_break = false};
 }
 
 typedef struct {
@@ -116,27 +71,46 @@ static StreamSet meanwhile(void (*f)(void*), void *ctx) {
 
 typedef void (*StreamAct)(int, void*, int pids[]);
 typedef struct {
-	StreamAct in, out, err;
-	void *in_ctx, *out_ctx, *err_ctx;
-} StreamActions;
+	char *in, *out, *err;
+} StreamFiles;
 
-static void handle_streams(StreamSet streams, StreamActions actions) {
-	StreamAct acts[3] = {actions.out, actions.err, actions.in};
-	void      *ctxs[3] = {actions.out_ctx, actions.err_ctx, actions.in_ctx};
-	int	  fds[3] = {streams.out, streams.err, streams.in};
-	pid_t	  pids[3];
+static void handle_streams(StreamSet streams, StreamFiles files) {
+	if(fork() != 0) return;
 
-	for(uint i = 0; i < 3; i++) {
-		pids[i] = fork();
-		if(pids[i] < 0)
-			die("fork failed")
-		else if(pids[i] == 0) {
-			for(uint j = 0; j < 3; j++)
-				if(j!=i) close(fds[j]);
+	static char buff[0xfff];
+	struct pollfd poll_data[6] = {
+		{ .fd = open(files.in, O_RDWR, 0),	.events = POLLIN },
+		{ .fd = streams.out,			.events = POLLIN },
+		{ .fd = streams.err,			.events = POLLIN },
 
-			acts[i](fds[i], ctxs[i], pids);
-			exit(0);
+		{ .fd = streams.in,			.events = POLLOUT },
+		{ .fd = open(files.out, O_RDWR, 0),	.events = POLLOUT },
+		{ .fd = open(files.err, O_RDWR, 0),	.events = POLLOUT }
+
+		// I use O_RDWR to avoid blocking on opening named pipe
+		// Poll will let to read/write to them until buffer is full
+	};
+
+	for(uint i = 0; i < 6; i++)
+		if(poll_data[i].fd < 0) {
+			fprintf(stderr, "box: cannot open stream %d.\n", i);
+			exit(1);
 		}
+
+	while(poll(poll_data, 6, 0) > 0) {
+		for (uint i = 0; i < 3; i++) {
+			if(poll_data[i].revents & POLLIN && poll_data[i+3].revents & POLLOUT) {
+				int bytes = read(poll_data[i].fd, buff, 0xfff);
+				write(poll_data[i+3].fd, buff, bytes);
+			}
+		}
+	}
+
+	perror("box/poll");
+	fprintf(stderr, "box: terminated");
+	
+	for(uint i = 0; i < 6; i++) {
+		close(poll_data[i].fd);
 	}
 }
 
@@ -146,38 +120,6 @@ static void execute(void *_args) {
 	execvp(args[0], args);
 	fprintf(stderr, "box: cannot run %s\n", args[0]);
 	exit(1);
-}
-
-static void handle_input(int fd, void *ctx, int pids[]){
-	while(1) {
-		int input = open((char*)ctx, O_RDONLY);
-		if(input <= 0)
-			die("open " input_fifo);
-
-		if(forward_stream(input, fd).write_break){
-			for(int i = 0; i < 2; i++) {
-				kill(pids[i], SIGTERM);
-			}
-			cleanup(0);
-		}
-
-		close(input);
-	}
-}
-
-static void handle_output(int fd, void *ctx, int pids[]) {
-	while(1) {
-		int output = open((char*)ctx, O_WRONLY);
-		if(output <= 0)
-			die("open " output_fifo);
-
-		if(forward_stream(fd, output).read_break){
-			fprintf(stderr, "box/output: read break\n");
-			exit(1);
-		}
-
-		close(output);
-	}
 }
 
 #define Signal_Handler(Sig, Handler) { \
@@ -191,7 +133,6 @@ int main(int argc, char **argv){
 	Signal_Handler(SIGINT, &cleanup);
 	Signal_Handler(SIGTERM, &cleanup);
 	Signal_Handler(SIGKILL, &cleanup);
-	Signal_Handler(SIGPIPE, &brk_pipe_handler);
 
 	if(argc < 2) {
 		fprintf(stderr,
@@ -212,10 +153,8 @@ int main(int argc, char **argv){
 	mkfifo(error_fifo, 0660);
 
 	StreamSet program_streams = meanwhile(&execute, (void*)args);
-	handle_streams(program_streams, (StreamActions){
-		.in = &handle_input, .in_ctx = input_fifo,
-		.out = &handle_output, .out_ctx = output_fifo,
-		.err = &handle_output, .err_ctx = error_fifo
+	handle_streams(program_streams, (StreamFiles){
+		.in = input_fifo, .out = output_fifo, .err = error_fifo
 	});
 
 	return 0;
